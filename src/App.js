c113e631fd6d29a9c9c922cb1199958ffeaa5e63
Jhosebro <jhosebro2108@gmail.com>
2023-12-19 23:10:19 -0500

    Add NavBar menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
 import Colegios from "./components/pages/Colegios";
-
-
+import NavBar from "./components/navigation/NavBar";
 
 function App() {
-  return(
-    <Colegios></Colegios>
-  )
+  return (
+    <>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="colegios" element={<Colegios />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
